0f5f1440895e2d1dc8c6168c6e78bb58b70fd967
Bruno Silva6 <bruno_silva6@dell.com>
2025-03-12 12:44:26 -0300

    Initialize the unit test classes

---
+++ tests/test_paddle.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestPaddle(unittest.TestCase):
+    def setUp(self):
+        """Set up the test environment"""
+        pass
+
+    def tearDown(self):
+        """Tear down after each test"""
+        pass
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
