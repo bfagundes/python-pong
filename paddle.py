4a02f6bc5960dd35c142ce5e56297ba50a76bc0c
Bruno Silva6 <bruno_silva6@dell.com>
2025-03-12 15:23:42 -0300

    Fixed paddles going out of screen bounds

--- paddle.py
+++ paddle.py
@@ -11,25 +11,27 @@ class Paddle:
         Args:
            x_position: The initial X position for the paddle
         """
+        self.base_size = 20
         self.width = 20
         self.length = 100
+        self.boundary_offset = (self.length / 2) + self.base_size
 
         self.paddle = Turtle()
         self.paddle.speed(0)
         self.paddle.shape("square")
         self.paddle.color("white")
-        self.paddle.shapesize(stretch_wid = self.length/20, stretch_len = self.width/20)
+        self.paddle.shapesize(stretch_wid = self.length/self.base_size, stretch_len = self.width/self.base_size)
         self.paddle.penup()
         self.paddle.goto(x_position, 0)
 
     def move_up(self):
         """Moves the Paddle UP"""
-        if self.paddle.ycor() < GRID_SIZE:
+        if self.paddle.ycor() < GRID_SIZE - self.boundary_offset:
             self.paddle.sety(self.paddle.ycor() + PADDLE_BASE_MOV_SPEED)
 
     def move_down(self):
         """Moves the Paddle DOWN"""
-        if self.paddle.ycor() > -GRID_SIZE:
+        if self.paddle.ycor() > -GRID_SIZE + self.boundary_offset:
             self.paddle.sety(self.paddle.ycor() - PADDLE_BASE_MOV_SPEED)
 
     def get_position(self):
