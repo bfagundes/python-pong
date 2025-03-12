a841823df2c6be4e7713af34da65ad4e7c2e98c5
Bruno Silva6 <bruno_silva6@dell.com>
2025-03-12 13:40:00 -0300

    Implemented Paddle

---
+++ paddle.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+from config import GRID_SIZE
+
+class Paddle:
+    def __init__(self, x_position):
+        self.paddle = Turtle()
+        self.paddle.speed(0)
+        self.paddle.shape("square")
+        self.paddle.color("white")
+        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
+        self.paddle.penup()
+        self.paddle.goto(x_position, 0)
+
+    def move_up(self):
+        if self.paddle.ycor() < GRID_SIZE:
+            self.paddle.sety(self.paddle.ycor() + 20)
+
+    def move_down(self):
+        if self.paddle.ycor() > -GRID_SIZE:
+            self.paddle.sety(self.paddle.ycor() - 20)
+
+    def get_position(self):
+        return self.paddle.xcor(), self.paddle.ycor()
+
+if __name__ == "__main__":
+    pass
--- tests/test_paddle.py
+++ tests/test_paddle.py
@@ -1,9 +1,34 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import unittest
+from paddle import Paddle
+from config import GRID_SIZE
 
 class TestPaddle(unittest.TestCase):
     def setUp(self):
         """Set up the test environment"""
-        pass
+        self.paddle = Paddle(x_position = 0)
+
+    def test_move_up(self):
+        initial_y = self.paddle.get_position()[1]
+        self.paddle.move_up()
+        self.assertEqual(self.paddle.get_position()[1], initial_y + 20, f"Paddle failed to move up correctly")
+
+    def test_move_down(self):
+        initial_y = self.paddle.get_position()[1]
+        self.paddle.move_down()
+        self.assertEqual(self.paddle.get_position()[1], initial_y - 20, f"Paddle failed to move down correctly")
+
+    def test_upper_boundary(self):
+        self.paddle.paddle.sety(GRID_SIZE)
+        self.paddle.move_up()
+        self.assertEqual(self.paddle.get_position()[1], GRID_SIZE, f"Paddle out of bounds on the upper boundary")
+
+    def test_lower_boundary(self):
+        self.paddle.paddle.sety(-GRID_SIZE)
+        self.paddle.move_down()
+        self.assertEqual(self.paddle.get_position()[1], -GRID_SIZE, f"Paddle out of bounds on the upper boundary")
 
     def tearDown(self):
         """Tear down after each test"""
