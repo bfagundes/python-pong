bbbece5a50ecd98e972f8b84d8afdbfda448721c
Bruno Silva6 <bruno_silva6@dell.com>
2025-03-13 13:08:36 -0300

    Added tests for Ball collision

--- ball.py
+++ ball.py
@@ -12,6 +12,7 @@ class Ball:
         self.ball.color("white")
         self.ball.penup()
         self.size = 20
+        self.boundary_offset = 10
 
         # Make it a little smaller
         self.ball.shapesize(stretch_wid=0.7, stretch_len=0.7)
@@ -28,23 +29,32 @@ class Ball:
     
     def move_up(self):
         """Moves the Ball UP"""
-        if self.ball.ycor() < GRID_SIZE - self.size:
+        if self.ball.ycor() < GRID_SIZE:
             self.ball.sety(self.ball.ycor() + BALL_BASE_MOV_SPEED)
 
     def move_down(self):
         """Moves the Ball DOWN"""
-        if self.ball.ycor() > -GRID_SIZE + self.size:
+        if self.ball.ycor() > -GRID_SIZE:
             self.ball.sety(self.ball.ycor() - BALL_BASE_MOV_SPEED)
 
     def move_left(self):
-        """Moves the Ball DOWN"""
-        if self.ball.xcor() > -GRID_SIZE + self.size:
+        """Moves the Ball LEFT"""
+        if self.ball.xcor() > -GRID_SIZE:
             self.ball.setx(self.ball.xcor() - BALL_BASE_MOV_SPEED)
 
     def move_right(self):
-        """Moves the Ball DOWN"""
-        if self.ball.xcor() > -GRID_SIZE + self.size:
-            self.ball.setx(self.ball.xcor() - BALL_BASE_MOV_SPEED)
+        """Moves the Ball RIGHT"""
+        if self.ball.xcor() < GRID_SIZE:
+            self.ball.setx(self.ball.xcor() + BALL_BASE_MOV_SPEED)
     
-    def is_goal_left(self):
+    def collision_left(self):
         return self.ball.xcor() >= -GRID_SIZE
+    
+    def collision_right(self):
+        return self.ball.xcor() <= GRID_SIZE
+
+    def collision_top(self):
+        return self.ball.ycor() <= GRID_SIZE
+
+    def collision_down(self):
+        return self.ball.ycor() >= - GRID_SIZE
